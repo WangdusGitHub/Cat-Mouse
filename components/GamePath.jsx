9dbc67e6c397dc540710bc62eedbb673b7f9f45f
Soanm Wangdsu <wangdus.las010@gamil.com>
2025-01-06 19:18:01 +0530

    Add tailwind, basic frontend

---
+++ components/GamePath.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./GamePath.css";
+import "../src/App.css";
+function GamePath() {
+  return (
+    <>
+      <div id="circle"></div>
+    </>
+  );
+}
+
+export default GamePath;
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
+import GamePath from "../components/GamePath";
 
 function App() {
   return (
     <>
-      <div className="wrap">
-        <div className="title flex center">Cat and Mouse</div>
+      <div className="wrap h-screen w-screen flex align-middle flex-col bg-zinc-800 text-white">
+        <div className="h-8 w-full flex justify-center align-middle text-2xl my-4">
+          Cat and Mouse
+        </div>
+
+        <div className="m-auto h-96 w-11/12 bg-zinc-700 p-4 rounded flex place-content-center">
+          <GamePath />
+        </div>
       </div>
     </>
   );
