05d8310559ca28a62404f8c1f02260e8f23a062b
Soanm Wangdsu <wangdus.las010@gamil.com>
2025-01-06 18:04:25 +0530

    Initialization of the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="wrap">
+        <div className="title flex center">Cat and Mouse</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
