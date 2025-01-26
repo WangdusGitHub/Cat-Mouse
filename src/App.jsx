5c814094ffa74e773f7a17db5ae13aee59ff7616
sonam wangdus <wangdus.las010@gmail.com>
2025-01-26 12:24:05 +0530

    modified GamePath.jsx, GamePath.css, App.jsx and App.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,14 @@ import GamePath from "../components/GamePath";
 
 function App() {
   return (
-    <div className="wrap">
-      <div className="title flex center">Cat and Mouse</div>
-      <div className="game flex center">
-        <div className="circle">
-          <div className="dot center"></div> {/* Center dot */}
-          {[...Array(5)].map((_, i) => (
-            <div key={i} className="dot" style={{ "--i": i }}></div>
-          ))}
+    <>
+      <div className="wrap">
+        <div className="gameTitle">Cat and Mouse</div>
+        <div className="gameBox">
+          <GamePath />
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
